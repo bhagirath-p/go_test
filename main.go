ecf56774a3d778af370307de773baa147ca4a8e5
Bhagirath <bpurushothama@qwinix.io>
2016-03-24 16:00:14 +0530

    modified code

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"github.com/gorilla/mux"
 	"github.com/bhagirath-p/go_test/api/v1/controllers"
 )
@@ -10,4 +11,7 @@ func main() {
 	r:=mux.NewRouter()
 	log.Println("here main")
 	r.HandleFunc("/sign_up", controllers.Register.Create).Methods("POST")
+	http.Handle("/", r)
+	log.Println("main : Started : Listening on : http://localhost:3000 ...")
+	log.Fatal(http.ListenAndServe("0.0.0.0:3000", nil))
 }
