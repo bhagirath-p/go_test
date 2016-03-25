4e01cf84934d3f505fff0ff3fd23ca62ccf11c3a
Bhagirath <bpurushothama@qwinix.io>
2016-03-25 09:23:30 +0530

    added model

--- api/v1/controllers/sign_up.go
+++ api/v1/controllers/sign_up.go
@@ -3,6 +3,9 @@ package controllers
 import (
 	"log"
 	"net/http"
+	"encoding/json"
+	"github.com/bhagirath-p/go_test/api/v1/models"
+	"io/ioutil"
 )
 
 type RegisterController struct {}
@@ -10,7 +13,16 @@ type RegisterController struct {}
 var Register RegisterController
 
 func (r RegisterController) Create(rw http.ResponseWriter, req *http.Request) {
-	log.Println("here")
+	var u models.User
+	req_body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	body := json.Unmarshal(req_body, &u)
+	if body != nil {
+		panic(err)
+	}
+	log.Printf("%v", u)
 }
 
 // func Create(req Request, rw ResponseWriter) {
